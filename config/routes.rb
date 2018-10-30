aa7be0a7a141b64e87e6346b0d01d6f995106534
handacg <handa.cheng@gmail.com>
2018-10-30 12:20:26 +0100

    fixed page does not  exist error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+	root 'presentation#home'
 	get '/team', to: 'presentation#team'
 	get '/contact', to: 'presentation#contact'
 	get 'home/:first_name', to: 'presentation#welcome'
 	get '/home', to: 'presentation#home'
+	get 'gossips/:id', to: 'gossip#show'
 end
